5bc5287187a99e9e08d811d0f2af4e895b6608ef
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-08-14 21:47:34 -0300

    add camera and suspense

--- src/app/sushi/page.tsx
+++ src/app/sushi/page.tsx
@@ -1,25 +1,24 @@
-'use client'
+"use client";
 
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Preload } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Scene from '~/scenes/01.sushi'
+import Scene from "~/scenes/01.sushi";
 
 export default function SushiPage() {
   return (
     <Canvas
-    style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100%',
-    }}>
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100%",
+      }}
+    >
       <Scene />
-      <mesh>
-        <boxGeometry attach="geometry" />
-        <meshStandardMaterial attach="material" color="#ac4d85" />
-      </mesh>
+      <PerspectiveCamera makeDefault position={[-5, 4, 7]} />
       <OrbitControls />
+      <Preload all />
     </Canvas>
-  )
+  );
 }
--- src/scenes/01.sushi.tsx
+++ src/scenes/01.sushi.tsx
@@ -1,21 +1,25 @@
 import { Gltf } from "@react-three/drei";
+import { Suspense } from "react";
 /**
  * Copyright (c) Michael Dougall. All rights reserved.
  *
  * This source code is licensed under the GPL-3.0 license found in the LICENSE
  * file in the root directory of this source tree.
  */
-import Box from "~/components/box";
-import Cylinder from "~/components/cylinder";
 
 export default function Scene() {
   return (
-    <>
+    <Suspense fallback={null}>
       <ambientLight intensity={0.25} />
       <pointLight
         castShadow={true}
         intensity={4.3}
-        position={[-3.7594113786689043,1.6377178989292671,1.122109130607944]} color={-0.84} decay={-0.02} frustumCulled={false} layers={-0.02} renderOrder={0.04}
+        position={[-3.7594113786689043, 1.6377178989292671, 1.122109130607944]}
+        color={-0.84}
+        decay={-0.02}
+        frustumCulled={false}
+        layers={-0.02}
+        renderOrder={0.04}
       />
       <pointLight
         castShadow
@@ -24,19 +28,13 @@ export default function Scene() {
       />
       <pointLight castShadow intensity={0.1} />
 
-      <group>
-        
-      </group>
+      <group></group>
 
-      
-      <Gltf src="/assets/sushi.glb" position={[-0.7553853988647461,0,0.38181358575820923]}/>
-      <mesh position={[0,0,0]} rotation={[-1.5707963267948966,0,0]}>
-        <planeGeometry args={[30,30]} /><meshStandardMaterial color={"#912b2b"} />
+      <Gltf src="/assets/sushi.glb" position={[-0.7553853988647461, 0, 0.38181358575820923]} />
+      <mesh position={[0, 0, 0]} rotation={[-1.5707963267948966, 0, 0]}>
+        <planeGeometry args={[30, 30]} />
+        <meshStandardMaterial color={"#912b2b"} />
       </mesh>
-      <mesh position={[0,0,0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"#ac4d85"} />
-      </mesh>
-    </>
+    </Suspense>
   );
 }
