d2d94438d327bd80115bad326c5b9b8a7742f820
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-08-25 18:05:05 -0300

    finished? scene

--- src/app/sushi/page.tsx
+++ src/app/sushi/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Html, Loader, Preload } from "@react-three/drei";
+import { Loader, Preload } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Scene from "~/scenes/01.sushi";
@@ -10,7 +10,7 @@ export default function SushiPage() {
     <div>
       <Canvas
         gl={{ antialias: false }}
-        camera={{ position: [-5, 4, 7], near: 0.1, far: 100, fov: 30 }}
+        camera={{ position: [-10, 13, 25], near: 0.1, far: 100, fov: 30 }}
         style={{
           position: "absolute",
           top: 0,
@@ -20,20 +20,15 @@ export default function SushiPage() {
         }}
         shadows
       >
-        <Suspense
-          fallback={
-            <Html>
-              <div>Loading...</div>
-            </Html>
-          }
-        >
+        <Suspense fallback={null}>
           <Scene />
         </Suspense>
         <Preload all />
       </Canvas>
       <Loader />
-      <article className="absolute p-4">
-        <h1>Sushi</h1>
+      <article className="absolute bottom-0 left-0 p-4 bg-black pointer-events-none">
+        <h2 className="text-md">Sushi</h2>
+        <p className="text-xs text-slate-200">Loading custom 3D model</p>
       </article>
     </div>
   );
--- src/scenes/01.sushi.tsx
+++ src/scenes/01.sushi.tsx
@@ -1,25 +1,60 @@
 import {
   AccumulativeShadows,
-  CameraControls,
   Environment,
+  PerspectiveCamera,
   RandomizedLight,
 } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { Bloom, EffectComposer, N8AO, Vignette } from "@react-three/postprocessing";
-import { Sushi } from "./model";
+import { Vector3 } from "three";
+import { SoySauceBottle } from "~/models/soySauceBottle";
+import { SoySauceGlass } from "~/models/soySauceGlass";
+import { Sushi } from "~/models/sushi";
+import { SushiPlate } from "~/models/sushiPlate";
+
+const CAMERA_POSITION = new Vector3(10, 18, -22);
+const TARGET_CAMERA_POSITION = new Vector3(10, 18, -22);
 
 export default function Scene() {
+  useFrame(({ pointer, camera }) => {
+    console.log(pointer.y);
+
+    const x = CAMERA_POSITION.x + pointer.x * 5;
+    const y = CAMERA_POSITION.y + pointer.y * 5;
+
+    TARGET_CAMERA_POSITION.set(x, y, camera.position.z);
+
+    camera.position.lerp(TARGET_CAMERA_POSITION, 0.1);
+    camera.lookAt(0, 0, 0);
+  });
+
   return (
     <>
-      <group>
-        <AccumulativeShadows color="blue" colorBlend={4} temporal frames={100} scale={25}>
-          <RandomizedLight radius={6} position={[-10, 9, -10]} />
-        </AccumulativeShadows>
+      <AccumulativeShadows
+        color="orange"
+        alphaTest={0.3}
+        colorBlend={4}
+        temporal
+        frames={100}
+        scale={25}
+      >
+        <RandomizedLight radius={6} position={[-10, 9, -10]} />
+      </AccumulativeShadows>
 
-        <Sushi scale={2} position={[0, 0, 0]} />
+      <SushiPlate scale={2} position={[0, 0, 0]} />
+      <SoySauceBottle position={[-6.5, 0, -3]} />
+      <SoySauceGlass position={[6, 0, 0]} />
+      <group position={[0.5, 0.1, -0.3]}>
+        <Sushi position={[-1.6, 0, 1.1]} />
+        <Sushi position={[-0.5, 0, 1.1]} />
+        <Sushi position={[0.6, 0, 1.1]} />
+        <Sushi position={[-1.6, 0, 0]} />
+        <Sushi position={[-0.5, 0, 0]} />
+        <Sushi position={[0.6, 0, 0]} />
       </group>
 
       <Environment preset="lobby" background blur={1} />
-      <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+      <PerspectiveCamera position={CAMERA_POSITION} fov={30} makeDefault />
 
       <EffectComposer disableNormalPass>
         <Bloom mipmapBlur intensity={1} luminanceThreshold={1} />
