6a2a2d0e430d2aa4f9137a11918251e540ef4c8a
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-09-20 18:21:41 -0300

    initial version of postselector component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
-import { Selector } from "~/components/selector";
+import { Canvas } from "@react-three/fiber";
+import type { AppProps } from "next/app";
+import { useEffect, useRef } from "react";
+import { Selector } from "~/components/canvas/selector";
+import { useStore } from "~/lib/store";
 
-export default function App({ Component, pageProps }) {
+import "~/styles/global.scss";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const dom = useRef<HTMLDivElement>(null!);
+
+  const { setDom } = useStore.getState();
+  useEffect(() => {
+    console.log("domElement", dom.current);
+    if (dom.current) setDom(dom.current);
+  }, [dom, setDom]);
+
   return (
-    <div>
-      <h1>App</h1>
-      <Selector />
+    <div ref={dom} className="relative h-screen w-full">
       <Component {...pageProps} />
+      <Canvas
+        className="!fixed h-full w-full top-0"
+        style={{ pointerEvents: "none" }}
+        eventSource={dom}
+        eventPrefix="client"
+      >
+        <Selector />
+      </Canvas>
     </div>
   );
 }
