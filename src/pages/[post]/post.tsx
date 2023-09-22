7423d8cd287db3b845fa2fbf0b9d67f39996ff44
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-09-22 00:55:26 -0300

    post slide animation

--- src/pages/[post]/post.tsx
+++ src/pages/[post]/post.tsx
@@ -2,11 +2,21 @@ import { Layout } from "~/components/layout";
 import { MDX } from "~/components/mdx";
 import { Props } from ".";
 
+import styles from "./post.module.scss";
+
 export function Post({ posts, mdx, metadata }: Props) {
   return (
-    <Layout posts={posts}>
-      <h1>{metadata.title}</h1>
-      <MDX source={mdx} />
+    <Layout
+      posts={posts}
+      className={styles.postWrapper}
+      initial={{ y: 0 }}
+      animate={{ y: -200 }}
+      exit={{ y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <article>
+        <MDX source={mdx} />
+      </article>
     </Layout>
   );
 }
