29d524c30d6ae4fa8ce9ce12391c0dd83489cc90
Emanuel Lorenzo <aemalorenzo@gmail.com>
2023-09-23 18:35:45 -0300

    canvas background

---
+++ src/components/canvas/background/index.tsx
@@ -0,0 +1,30 @@
+import { useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+import { useStore } from "~/lib/store";
+
+type Props = JSX.IntrinsicElements["mesh"];
+
+export function Background({ position, ...props }: Props) {
+  const background = useRef<THREE.Mesh>(null!);
+  const { setSelectedPostIndex } = useStore.getState();
+
+  const viewport = useThree((t) => t.viewport);
+  const camera = useThree((t) => t.camera) as THREE.PerspectiveCamera;
+
+  const offsetViewport = viewport.getCurrentViewport(camera, position as THREE.Vector3);
+
+  return (
+    <mesh
+      ref={background}
+      position={position}
+      onClick={() => {
+        setSelectedPostIndex(null);
+      }}
+      {...props}
+    >
+      <planeGeometry args={[offsetViewport.width, offsetViewport.height, 1, 1]} />
+      <meshBasicMaterial transparent opacity={0} />
+    </mesh>
+  );
+}
